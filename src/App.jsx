cad5597d11627f336ce57ca6da32cf7abe252c7c
Sullivan Berger <sullivan.berger@gmail.com>
2023-09-16 12:36:44 +0200

    Finish exercises 1.1 & 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
-const Hello = (props) => {
-
-    console.log(props)
-    return (
-        <div>
-            <p>
-
-                Hello {props.name}, you are {props.age} years old
-            </p>
-        </div>
-    )
-}
+import Header from "./components/Header.jsx";
+import Content from "./components/Content.jsx";
+import Total from "./components/Total.jsx";
 
 const App = () => {
+    const course = 'Half Stack application development'
 
-    const name = 'Peter'
-    const age = 10
 
     return (
         <div>
-            <h1>Greetings</h1>
-
-            <Hello name='Maya' age={26 + 10} />
-            <Hello name={name} age={age} />
+            <Header course={course} />
+            <Content />
+            <Total />
         </div>
     )
 }
